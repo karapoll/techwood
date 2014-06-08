6a2499e1a4b06eb3bd7f096daf26e5475cf01a8c
karapoll <karim.apollinaire@gmx.net>
2014-06-08 12:46:26 +0200

    init techwood

---
+++ javascript/jquery-extend-core.js
@@ -0,0 +1,78 @@
+﻿// JavaScript Document
+//---------------------------------------------------------------------------------------------
+// QUICKEACH METHOD : ROULETTE SOURIS
+// © 2010 - James Padolsey - http://james.padolsey.com/
+// jQuery quickEach() est environ 88% plus rapide que la méthode originale jQuery each()
+
+(function(a){a.fn.quickEach=function(g){var d=a([0]),e=-1,b=this.length,h;while(++e<b&&(h=d[0]=this[e])&&g.call(d,e,h)!==false){}return this}})(jQuery);
+
+//---------------------------------------------------------------------------------------------
+// FONCTIONS GENERIQUES
+var px=function(x){return parseFloat(x)+'px'},inv=function(x){return 1/x};
+var pad = function(n,l){var s=''+n;while(s.length<l){s='0'+s;}return s;};
+
+//---------------------------------------------------------------------------------------------
+// TIMER (Object JS)
+// Copyright © 2011 - Romain Saillant (+ sources diverses)
+
+var Timer=function(c,e){this.interval=parseFloat(c)*1000;this.played=false;e=(e!=undefined)?e:9e+99;var d=0;this.intCounter=function(){return d};var a=0;var b;this.action;this.start=this.play=function(){this.played=true;b=this;b.timerId=setInterval(function(){d++;b.action();if(d==e){b.stop()}},b.interval)};this.stop=function(){this.played=false;clearInterval(b.timerId);d=0};this.pause=function(){this.played=false;clearInterval(b.timerId)};this.resume=function(){this.played=true;if(d!=0&&this.played==true){b.start()}};this.playPause=function(){if(this.played==true){b.pause();this.played=false}else{b.play();this.played=true}};this.afterAction=function(){this.played=true;b=this;b.timerId=setInterval(function(){d++;if(d==e){b.stop();b.action()}},b.interval)}};
+
+//----------------------------------------------------------------------------------------------
+// RECUPERATION DES VARIABLES D'URL
+// © 2006 - Roshambo - http://snipplr.com/view/799/get-url-variables/
+// ex. URL : http://www.monsite.com/mapage.html?cat=26
+// ex. JS  : getUrlVars()['cat'] => 26
+
+var getUrlVars=function(){var d=[],c;var a=window.location.href.slice(window.location.href.indexOf("?")+1).split("&");for(var b=0;b<a.length;b++){c=a[b].split("=");d.push(c[0]);d[c[0]]=c[1]}return d};
+
+//---------------------------------------------------------------------------------------------
+// DIMENSIONS FENETRE
+var dimScreen = {
+	width:0,
+	height:0	
+}
+// (Re)Calcul des dimmensions au chargement de la fenêtre et en cas de redimensionnement de cette dernière
+$(window).on('load resize',function(e){
+	dimScreen.width = $(window).innerWidth();
+	dimScreen.height = $(window).innerHeight();	
+});
+//---------------------------------------------------------------------------------------------
+// EVENT RESIZEEND -> Déclenchement d'un event à la fin d'un resize de la fenêtre du navigateur
+// Adapté d'une contribution de Carlos Martinez - le 2 octobre 2012 - Forum : http://stackoverflow.com/
+
+// Ex.: $(window).on('resizeEnd',function(){ window.location.reload(); }); // Rechargement de la page APRÈS un resize.
+
+$(window).on('resize',function(){
+	if(this.resized){
+		clearTimeout(this.resized)
+	};
+	this.resized = setTimeout(function(){
+		$(this).triggerHandler('resizeEnd');
+	},500);
+});
+//---------------------------------------------------------------------------------------------
+// CALCUL DU NOMBRE D'ENTREES DANS UN OBJET NATIF
+// ex. : var max = Object.size(monObjectJS);
+
+	Object.size = function(obj) {
+		var size = 0, key;
+		for(key in obj){if(obj.hasOwnProperty(key)) size++;}
+		return size;
+	};
+//---------------------------------------------------------------------------------------------
+// FONCTION SUBSTR (idem PHP) ex.: substr('abcdef',0,-1); © http://phpjs.org/functions/substr
+
+var substr = function(k,a,g){var f=0,c=true,m=0,b=0,j=0,h="";k+="";var e=k.length;this.php_js=this.php_js||{};this.php_js.ini=this.php_js.ini||{};switch((this.php_js.ini["unicode.semantics"]&&this.php_js.ini["unicode.semantics"].local_value.toLowerCase())){case"on":for(f=0;f<k.length;f++){if(/[\uD800-\uDBFF]/.test(k.charAt(f))&&/[\uDC00-\uDFFF]/.test(k.charAt(f+1))){c=false;break}}if(!c){if(a<0){for(f=e-1,m=(a+=e);f>=m;f--){if(/[\uDC00-\uDFFF]/.test(k.charAt(f))&&/[\uD800-\uDBFF]/.test(k.charAt(f-1))){a--;m--}}}else{var d=/[\uD800-\uDBFF][\uDC00-\uDFFF]/g;while((d.exec(k))!=null){var l=d.lastIndex;if(l-2<a){a++}else{break}}}if(a>=e||a<0){return false}if(g<0){for(f=e-1,b=(e+=g);f>=b;f--){if(/[\uDC00-\uDFFF]/.test(k.charAt(f))&&/[\uD800-\uDBFF]/.test(k.charAt(f-1))){e--;b--}}if(a>e){return false}return k.slice(a,e)}else{j=a+g;for(f=a;f<j;f++){h+=k.charAt(f);if(/[\uD800-\uDBFF]/.test(k.charAt(f))&&/[\uDC00-\uDFFF]/.test(k.charAt(f+1))){j++}}return h}break}case"off":default:if(a<0){a+=e}e=typeof g==="undefined"?e:(g<0?g+e:g+a);return a>=k.length||a<0||a>e?!1:k.slice(a,e)}return undefined};
+
+//---------------------------------------------------------------------------------------------
+// FONCTION FILE_GET_CONTENTS (idem PHP) © http://phpjs.org/functions/file_get_contents/
+// Retourne le contenu du fichier passé en paramètre...
+
+// Exemple : file_get_contents('http://kevin.vanzonneveld.net/pj_test_supportfile_1.htm')
+
+var file_get_contents = function (l,s,f,p,j){var J,c=[],z=[],A=0,B=0,C="",q=-1,d=0,y=null,F=false;var o=function(e){return e.substring(1)!==""};this.php_js=this.php_js||{};this.php_js.ini=this.php_js.ini||{};var r=this.php_js.ini;f=f||this.php_js.default_streams_context||null;if(!s){s=0}var I={FILE_USE_INCLUDE_PATH:1,FILE_TEXT:32,FILE_BINARY:64};if(typeof s==="number"){d=s}else{s=[].concat(s);for(B=0;B<s.length;B++){if(I[s[B]]){d=d|I[s[B]]}}}if(d&I.FILE_BINARY&&(d&I.FILE_TEXT)){throw"You cannot pass both FILE_BINARY and FILE_TEXT to file_get_contents()"}if((d&I.FILE_USE_INCLUDE_PATH)&&r.include_path&&r.include_path.local_value){var x=r.include_path.local_value.indexOf("/")!==-1?"/":"\\";l=r.include_path.local_value+x+l}else{if(!/^(https?|file):/.test(l)){C=this.window.location.href;q=l.indexOf("/")===0?C.indexOf("/",8)-1:C.lastIndexOf("/");l=C.slice(0,q+1)+l}}if(f){var w=f.stream_options&&f.stream_options.http;F=!!w}if(!f||F){var b=this.window.ActiveXObject?new ActiveXObject("Microsoft.XMLHTTP"):new XMLHttpRequest();if(!b){throw new Error("XMLHttpRequest not supported")}var g=F?w.method:"GET";var n=!!(f&&f.stream_params&&f.stream_params["phpjs.async"]);if(r["phpjs.ajaxBypassCache"]&&r["phpjs.ajaxBypassCache"].local_value){l+=(l.match(/\?/)==null?"?":"&")+(new Date()).getTime()}b.open(g,l,n);if(n){var a=f.stream_params.notification;if(typeof a==="function"){if(0&&b.addEventListener){}else{b.onreadystatechange=function(e){var i={responseText:b.responseText,responseXML:b.responseXML,status:b.status,statusText:b.statusText,readyState:b.readyState,evt:e};var k;switch(b.readyState){case 0:a.call(i,0,0,"",0,0,0);break;case 1:a.call(i,0,0,"",0,0,0);break;case 2:a.call(i,0,0,"",0,0,0);break;case 3:k=b.responseText.length*2;a.call(i,7,0,"",0,k,0);break;case 4:if(b.status>=200&&b.status<400){k=b.responseText.length*2;a.call(i,8,0,"",b.status,k,0)}else{if(b.status===403){a.call(i,10,2,"",b.status,0,0)}else{a.call(i,9,2,"",b.status,0,0)}}break;default:throw"Unrecognized ready state for file_get_contents()"}}}}}if(F){var H=w.header&&w.header.split(/\r?\n/);var v=false;for(B=0;B<H.length;B++){var E=H[B];var D=E.search(/:\s*/);var m=E.substring(0,D);b.setRequestHeader(m,E.substring(D+1));if(m==="User-Agent"){v=true}}if(!v){var t=w.user_agent||(r.user_agent&&r.user_agent.local_value);if(t){b.setRequestHeader("User-Agent",t)}}y=w.content||null}if(d&I.FILE_TEXT){var u="text/html";if(w&&w["phpjs.override"]){u=w["phpjs.override"]}else{var h=(r["unicode.stream_encoding"]&&r["unicode.stream_encoding"].local_value)||"UTF-8";if(w&&w.header&&(/^content-type:/im).test(w.header)){u=w.header.match(/^content-type:\s*(.*)$/im)[1]}if(!(/;\s*charset=/).test(u)){u+="; charset="+h}}b.overrideMimeType(u)}else{if(d&I.FILE_BINARY){b.overrideMimeType("text/plain; charset=x-user-defined")}}try{if(w&&w["phpjs.sendAsBinary"]){b.sendAsBinary(y)}else{b.send(y)}}catch(G){return false}J=b.getAllResponseHeaders();if(J){J=J.split("\n");for(A=0;A<J.length;A++){if(o(J[A])){z.push(J[A])}}J=z;for(B=0;B<J.length;B++){c[B]=J[B]}this.$http_response_header=c}if(p||j){if(j){return b.responseText.substr(p||0,j)}return b.responseText.substr(p)}return b.responseText}return false};
+
+//---------------------------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------------------------
---
+++ javascript/jquery-extend-mouse.js
@@ -0,0 +1,386 @@
+﻿// JavaScript Document
+//---------------------------------------------------------------------------------------------
+// MOUSEWHEEL
+//---------------------------------------------------------------------------------------------
+// MOUSEWHEEL EVENT : ROULETTE SOURIS
+// © 2011 Brandon Aaron (http://brandonaaron.net)
+// Version: 3.0.6 - Requires: jQuery : 1.2.2+
+
+(function(d){var e=["DOMMouseScroll","mousewheel"];if(d.event.fixHooks){for(var a=e.length;a;){d.event.fixHooks[e[--a]]=d.event.mouseHooks}}d.event.special.mousewheel={setup:function(){if(this.addEventListener){for(var c=e.length;c;){this.addEventListener(e[--c],b,false)}}else{this.onmousewheel=b}},teardown:function(){if(this.removeEventListener){for(var c=e.length;c;){this.removeEventListener(e[--c],b,false)}}else{this.onmousewheel=null}}};d.fn.extend({mousewheel:function(c){return c?this.bind("mousewheel",c):this.trigger("mousewheel")},unmousewheel:function(c){return this.unbind("mousewheel",c)}});function b(h){var f=h||window.event,i=[].slice.call(arguments,1),k=0,j=true,g=0,c=0;h=d.event.fix(f);h.type="mousewheel";if(f.wheelDelta){k=f.wheelDelta/120}if(f.detail){k=-f.detail/3}c=k;if(f.axis!==undefined&&f.axis===f.HORIZONTAL_AXIS){c=0;g=-1*k}if(f.wheelDeltaY!==undefined){c=f.wheelDeltaY/120}if(f.wheelDeltaX!==undefined){g=-1*f.wheelDeltaX/120}i.unshift(h,k,g,c);return(d.event.dispatch||d.event.handle).apply(this,i)}})(jQuery);
+
+// * 0.008333 => /120
+// * 0.333333 => /3
+
+//---------------------------------------------------------------------------------------------
+// SCROLLSTART & SCROLLSTOP EVENTS : début et fin d'un scroll (un vrai scroll HTML => CSS:overflow -> auto)
+// © 2009 James Padolsey (http://james.padolsey.com/)
+
+// (function(d){var a=jQuery.event.special,c="D"+(+new Date()),b="D"+(+new Date()+1);a.scrollstart={setup:function(){var f,e=function(i){var g=this,h=arguments;if(f){clearTimeout(f)}else{i.type="scrollstart";jQuery.event.handle.apply(g,h)}f=setTimeout(function(){f=null},a.scrollstop.latency)};jQuery(this).on("scroll",e).data(c,e)},teardown:function(){jQuery(this).off("scroll",jQuery(this).data(c))}};a.scrollstop={latency:300,setup:function(){var f,e=function(i){var g=this,h=arguments;if(f){clearTimeout(f)}f=setTimeout(function(){f=null;i.type="scrollstop";jQuery.event.handle.apply(g,h)},a.scrollstop.latency)};jQuery(this).on("scroll",e).data(b,e)},teardown:function(){jQuery(this).off("scroll",jQuery(this).data(b))}}})(jQuery);
+
+//---------------------------------------------------------------------------------------------
+// DECLENCHEMENT D'UN .ON() EVENT ou DE LA FONCTION D'UNE METHODE EVENT
+(function($){
+	$.f_triggerEvent = $.fte = function(c,b,a){if($.isFunction(b)){b()}else{$(c.target).triggerHandler(a)}};
+	
+	/*
+	L'utilisation de la syntaxe $.maFonction ajoute la dite fonction à jQuery...
+	On peut faire de même avec une variable : ex. $.jour = 'lundi';
+	
+	ex. : $.f_triggerEvent(e,f,'altclick');
+	e : event Object => e.data.xxx , e.keyCode , e.target , e.currentTarget , ...
+	f : fonction passée en paramètre
+	a : nom de l'event => 'shiftclick'
+	
+	var f_triggerEvent = function(e,f,trigger){
+		if($.isFunction(f)){ // Si une fonction est passée en argument, on est alors dans une méthode de type leftclick()
+			f(); // Exécution de la fonction passée en paramètre.
+		}
+		else{ 
+			// Si une fonction n'est pas passée en paramètre, alors :
+			// Déclenchement d'un événement pour les méthodes on()/off()
+			$(e.target).triggerHandler(trigger); 
+		}
+	};
+	*/
+})(jQuery);
+//---------------------------------------------------------------------------------------------
+// CLICK
+//---------------------------------------------------------------------------------------------
+// CLICK UPDATED : ajout du cursor:pointer sur élément cliquable.
+// © 2010 - Ben Alman (http://benalman.com/news/2010/03/jquery-special-events/)
+(function(a){a.event.special.click={setup:function(){a(this).css('cursor','pointer');return false},teardown:function(){a(this).css('cursor','');return false}}})(jQuery);
+
+//----------------------------------------------------------------------------------------------
+// CLICKOUTSIDE EVENT : détection d'un clic de souris à l'extérieur d'un élément
+// © 2010 Ben Alman (http://benalman.com) - 2012 Updated by Romain Saillant (on() & off() method) - Requires : jQuery : 1.7+
+
+(function(c){var a=c([]);c.event.special.clickoutside={setup:function(){a=a.add(this);if(a.length===1){c(document).on('click',b)}},teardown:function(){a=a.not(this);if(a.length===0){c(document).off('click',b)}},add:function(d){var e=d.handler;d.handler=function(g,f){g.target=f;e.apply(this,arguments)}}};function b(d){c(a).each(function(){var e=c(this);if(this!==d.target&&!e.has(d.target).length){e.triggerHandler("clickoutside",[d.target])}})}})(jQuery);
+
+//---------------------------------------------------------------------------------------------
+// EVENTS : de ALTCLICK à ALTSHIFTCTRLCLICK : 
+// © 2012 Romain Saillant - Requires : jQuery : 1.7+
+// NOTE : Ne peut pas être combiné avec un event CLICK standard... sinon ce CLICK standard sera déclenché (puisque détecté) -> (***)
+
+(function($){
+	//-------------------------------------------------------
+	var events_ASC = ['altclick','shiftclick','ctrlclick','altshiftclick','altctrlclick','shiftctrlclick','altshiftctrlclick','metaclick','strictclick'];
+	//-------------------------------------------------------
+	var f_ASC_Click = function(e,f){ // Sous condition, exécution de la fonction $.f_triggerEvent() (alias $.fte())
+	
+		// Désactivation de tous menus contextuels déclenchés - par défaut - par le navigateur.
+		$(e.target).on('contextmenu',function(e){ return false; });
+
+		if     ( e.altKey && !e.shiftKey && !e.ctrlKey && !e.metaKey){ $.fte(e,f,events_ASC[0]); }
+		else if(!e.altKey &&  e.shiftKey && !e.ctrlKey && !e.metaKey){ $.fte(e,f,events_ASC[1]); }
+		else if(!e.altKey && !e.shiftKey &&  e.ctrlKey && !e.metaKey){ $.fte(e,f,events_ASC[2]); }
+		else if( e.altKey &&  e.shiftKey && !e.ctrlKey && !e.metaKey){ $.fte(e,f,events_ASC[3]); }
+		else if( e.altKey && !e.shiftKey &&  e.ctrlKey && !e.metaKey){ $.fte(e,f,events_ASC[4]); }
+		else if(!e.altKey &&  e.shiftKey &&  e.ctrlKey && !e.metaKey){ $.fte(e,f,events_ASC[5]); }
+		else if( e.altKey &&  e.shiftKey &&  e.ctrlKey && !e.metaKey){ $.fte(e,f,events_ASC[6]); }
+		else if(!e.altKey && !e.shiftKey && !e.ctrlKey &&  e.metaKey){ $.fte(e,f,events_ASC[7]); }
+		else if(!e.altKey && !e.shiftKey && !e.ctrlKey && !e.metaKey){ $.fte(e,f,events_ASC[8]); }
+	};	
+	//-------------------------------------------------------
+	for(i=0,max=events_ASC.length;i<max;i++){
+		// Création d'un nouvel event pour les méthodes on()/off() et d'une nouvelle méthode
+		// [A] EVENT
+		$.event.special[events_ASC[i]] = {  // [A] event pour la méthode on()
+		  setup:    function(){$(this).css('cursor','pointer').on('mouseup',f_ASC_Click);},
+		  teardown: function(){$(this).off('mouseup');}
+		};
+		// [B] METHODE
+		$.fn[events_ASC[i]] = function(f){  // [B] méthode altclick()
+			return this.quickEach(function(){
+				$(this).css('cursor','pointer').on('mouseup',function(e){f_ASC_Click(e,f);});
+			});
+		};
+	}
+	/*
+	//-------------------------------------------------------
+	$.event.special.altclick = {  // [A] event pour la méthode on()
+		setup:    function(){$(this).css('cursor','pointer').on('mouseup',f_ASC_Click);},
+		teardown: function(){$(this).off('mouseup');}
+	};
+	//-------------------------------------------------------
+	$.fn.altclick = function(f){  // [B] méthode altclick()
+		return this.quickEach(function(){
+			$(this).css('cursor','pointer').on('mouseup',function(e){f_ASC_Click(e,f);});
+		});
+	};
+	//-------------------------------------------------------
+	*/
+})(jQuery);
+
+
+//---------------------------------------------------------------------------------------------
+// EVENTS : LEFTCLICK - MIDDLECLICK - RIGHTCLICK 
+// © 2012 Romain Saillant - Requires : jQuery : 1.7+
+// NOTE : Ne peuvent pas être combiné avec un event CLICK standard... -> (***)
+
+(function($){
+	//-------------------------------------------------------
+	var events_BTN = ['leftclick','middleclick','rightclick'];
+	// ------------------------------------------------------
+	var f_mousebuttonclick = function(e,f){
+		// Désactivation de tous menus contextuels déclenchés - par défaut - par le navigateur.
+		$(e.target).on('contextmenu',function(e){ return false; });
+		$.f_triggerEvent(e,f,events_BTN[e.which-1]);
+	};
+	//-------------------------------------------------------
+	//var btn_num;
+	$.each(events_BTN,function(i,events){
+		//console.log(i);
+		// Création d'un nouvel event pour les méthodes on()/off() et d'une nouvelle méthode
+		// [A] EVENT
+		$.event.special[events] = {  // [A] event pour la méthode on()
+		//$.event.special[events_BTN[i]] = {  // [A] event pour la méthode on()
+		  setup:    function(){$(this).css('cursor','pointer').on('mouseup',{button:(i+1)},f_mousebuttonclick);},
+		  teardown: function(){$(this).off('mouseup');}
+		};
+		// [B] METHODE
+		$.fn[events] = function(f){  // [B] méthode altclick()
+		//$.fn[events_BTN[i]] = function(f){  // [B] méthode altclick()
+			return this.quickEach(function(){
+				$(this).css('cursor','pointer').on('mouseup',function(e){f_mousebuttonclick(e,f);});
+			});
+		};
+	});
+	//-------------------------------------------------------
+})(jQuery);
+
+
+//---------------------------------------------------------------------------------------------
+// EVENTS : SLOWCLICK - FASTCLICK : 
+// © 2012 Romain Saillant - Requires : jQuery : 1.7+
+// NOTE : Ne peut pas être combiné avec un event CLICK standard...
+
+(function($){
+	$.event.special.slowclick = {
+	  setup:    function(){$(this).css('cursor','pointer').on('mousedown',{speed:750,slow:true},f_speedclick);},
+	  teardown: function(){$(this).off('mousedown');}
+	};
+	//-------------------------------------------------------
+	$.event.special.fastclick = {
+	  setup:    function(){$(this).css('cursor','pointer').on('mousedown',{speed:150,slow:false},f_speedclick);},
+	  teardown: function(){$(this).off('mousedown');}
+	};
+	//-------------------------------------------------------
+	var f_speedclick = function(e,f){
+
+		// Désactivation de tous menus contextuels déclenchés - par défaut - par le navigateur.
+		$(e.target).on('contextmenu',function(e){ return false; });
+
+		var speed = e.data.speed,
+			slow =  e.data.slow,
+			time1 = e.timeStamp; // stockage du temps Unix au moment du mousedown.
+			
+		$(e.target).on('mouseup',function(e){ 
+			e.stopImmediatePropagation(); // évite le déclenchement de multiples onmouseup
+			time2 = e.timeStamp; // stockage du temps Unix au moment du mouseup.
+			if((time2 - time1) > speed && slow === true){ // Si la différence de temps est supérieure à 750ms
+				$.fte(e,f,'slowclick');
+			}
+			else if((time2 - time1) < speed && slow === false){ // Si la différence de temps est inférieure à 150ms
+				$.fte(e,f,'fastclick');
+			}
+		});
+	};	
+	//-------------------------------------------------------
+		$.fn.slowclick = function(f){
+			return this.quickEach(function(){
+				$(this).css('cursor','pointer').on('mousedown',{speed:750,slow:true},function(e){f_speedclick(e,f);});});
+		};
+	//-------------------------------------------------------
+		$.fn.fastclick = function(f){
+			return this.quickEach(function(){
+				$(this).css('cursor','pointer').on('mousedown',{speed:150,slow:false},function(e){f_speedclick(e,f);});});
+		};
+	//-------------------------------------------------------
+})(jQuery);
+
+//---------------------------------------------------------------------------------------------
+// KEYCLICK - ALTKEYCLICK - SHIFTKEYCLICK - CTRLKEYCLICK : 
+// © 2012 Romain Saillant - Requires : jQuery : 1.7+
+
+(function($){
+	var keyboard = {
+		// Specials :
+		27: 'esc', 9: 'tab', 32:'space', 13: 'return', 16: 'shift', 17: 'ctrl', 18: 'alt', 8:'backspace', 
+        145: 'scroll', 20: 'capslock', 144: 'numlock', 19:'pause', 45:'insert', 36:'home', 46:'del',
+        35:'end', 
+		// Directions :
+		33: 'pageup', 34:'pagedown', 37:'left', 38:'up', 39:'right',40:'down', 
+		// Numbers Keyboard :
+		48:'key0', 49:'key1', 50:'key2', 51:'key3', 52:'key4', 
+		53:'key5', 54:'key6', 55:'key7', 56:'key8', 57:'key9',
+		// Letters :
+		65:'a',66:'b',67:'c',68:'d',69:'e',70:'f',71:'g',72:'h',73:'i',74:'j',75:'k',76:'l',77:'m',
+		78:'n',79:'o',80:'p',81:'q',82:'r',83:'s',84:'t',85:'u',86:'v',87:'w',88:'x',89:'y',90:'z',
+		// Numbers Pad :
+		96: 'num0', 97: 'num1', 98: 'num2', 99: 'num3', 100:'num4', 
+		101:'num5', 102:'num6', 103:'num7', 104:'num8', 105:'num9',
+		// Signs :
+        106: 'num*', 107: 'num+', 108: '????', 109: 'num-', 110: '????', 111: 'Num/',
+		// F-Touchs :
+        112:'f1',113:'f2', 114:'f3', 115:'f4', 116:'f5', 117:'f6', 118:'f7', 119:'f8',
+		120:'f9', 121:'f10', 122:'f11' /* 123:'f12', 191: '/' , */ 
+	};
+	//-------------------------------------------------------
+	var keycode, // Initialisation du code ASCII de la touche enfoncée
+		f_keyclick = function(obj,ev,keySign,f){
+
+		// Désactivation de tous menus contextuels déclenchés - par défaut - par le navigateur.
+		$(obj).on('contextmenu',function(e){ return false; });
+		
+		// Récupération du code ASCII de la touche enfoncée... e.keyCode
+		$(document).on('keydown',function(e){ keycode = e.keyCode; }); 
+		
+		$(obj).css('cursor','pointer').on(ev,function(e){// Sur l'événement (ev) choisi...
+		// Si la valeur (dans l'Object keyboard) du code ASCII détecté correspond à la valeur envoyée (keySign.touch) => si 'a' === 'a'
+			if(keyboard[keycode] === keySign.touch){ 
+				if($.isFunction(f)){ // Si f est bien une fonction 
+					f(); // => Exécution de la fonction passée en paramétre des méthodes [B] keyclick() & cie.
+				}
+			}
+		});
+
+	};	
+    //-------------------------------------------------------
+//	$.fn.key = function(keySign,f){  // [B] méthode keyclick()
+//		return this.quickEach(function(){
+//			$(this).on('keyup',function(e){
+//				if(keyboard[e.keyCode] === keySign.touch){
+//					if($.isFunction(f)){ // Si f est bien une fonction 
+//						f(); // => Exécution de la fonction passée en paramétre des méthodes [B] keyclick() & cie.
+//					}
+//				}
+//			});
+//		});
+//	};
+    //-------------------------------------------------------
+	$.fn.keyclick = function(keySign,f){  // [B] méthode keyclick()
+		return this.quickEach(function(){
+			f_keyclick(this,'click',keySign,f);
+		});
+	};
+    //-------------------------------------------------------
+//	$.fn.altkey = function(keySign,f){  // [B] méthode keyclick()
+//		return this.quickEach(function(){
+//			$(this).on('keyup',function(e){
+//				if(e.altKey && keyboard[e.keyCode] === keySign.touch){
+//					if($.isFunction(f)){ // Si f est bien une fonction 
+//						f(); // => Exécution de la fonction passée en paramétre des méthodes [B] keyclick() & cie.
+//					}
+//				}
+//			});
+//		});
+//	};
+    //-------------------------------------------------------
+	$.fn.altkeyclick = function(keySign,f){  // [B] méthode altkeyclick()
+		return this.quickEach(function(){
+			f_keyclick(this,'altclick',keySign,f);
+		});
+	};
+	//-------------------------------------------------------
+	$.fn.shiftkeyclick = function(keySign,f){  // [B] méthode shiftkeyclick()
+		return this.quickEach(function(){
+			f_keyclick(this,'shiftclick',keySign,f);
+		});
+	};
+	//-------------------------------------------------------
+	$.fn.ctrlkeyclick = function(keySign,f){  // [B] méthode ctrlkeyclick()
+		return this.quickEach(function(){
+			f_keyclick(this,'ctrlclick',keySign,f);
+		});
+	};
+	//-------------------------------------------------------
+})(jQuery);
+
+//---------------------------------------------------------------------------------------------
+// DELAY - MOUSEHOLD :
+// © 2012 Romain Saillant - Requires : jQuery : 1.7+
+
+(function($){
+	//-------------------------------------------------------
+	$.f_eventdelay = function(obj,evtsDelay,params){
+		return obj.quickEach(function(){
+			//console.log(evDelay);
+			var p = $.extend({},{
+						repeat:1,
+						delay:0,
+						beforeAction:function(){},
+						afterAction: function(){},
+						action:      function(){}
+					},params); // Valeurs par defaut
+					
+			var	t = new Timer(p.delay,p.repeat); // 1 fois X secondes
+				
+			obj.css('cursor','pointer').on(evtsDelay.eventStart,function(e){
+				p.beforeAction();
+				t.afterAction = p.afterAction;
+				t.action = p.action;
+				t.start();
+			});
+			// Annule t.action si un second événement (mouseup, blur) intevient avant la fin du délai (p.delay)
+			if(evtsDelay.eventStopDelay !== null){
+				obj.on(evtsDelay.eventStopDelay,function(e){t.stop();});
+			}
+		});
+	};
+	//-------------------------------------------------------
+	$.fn.extend({
+		mousehold:function(params){
+			$.f_eventdelay(this,{eventStart:'mousedown',eventEnd:'mouseup'},params);
+		},
+		delayEvent : function(evtsDelay,params){
+			$.f_eventdelay(this,evtsDelay,params);
+		}
+	});
+	//-------------------------------------------------------
+})(jQuery);
+
+//---------------------------------------------------------------------------------------------
+// TEXT SELECTED :
+// © 2012 Romain Saillant - Requires : jQuery : 1.7+
+
+(function($){
+	//-------------------------------------------------------
+	$.textSelected; // Défaut, pourra ainsi être utilisé à posteriori par la méthode textSelect()
+	//-------------------------------------------------------
+	$.f_textSelected = function (){
+		var windoc = window || document;
+		if(windoc.getSelection){ // IE9 et W3C 
+			return windoc.getSelection().toString();
+		}
+		else if(document.selection){ // IE6/7/8
+			var s = document.selection.createRange();
+			return s.text;
+		}
+	}
+	//-------------------------------------------------------
+	$.fn.textSelect = function(f){
+		return this.quickEach(function(){
+			$(this).on('mouseup',function(e){
+				$.textSelected = $.f_textSelected();
+				// Protège l'exécution de la fonction de l'événement double-clic ( => 2x mouseup => le 1er est tjrs vide)
+				if($.textSelected !== ''){ 
+					f();
+				}
+			});
+		});
+	};
+	//-------------------------------------------------------
+})(jQuery);
+//---------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------
